7025fe6bf6725cf47ad47d973676a8781b4b7e43
Andrey Sobolev <andrey.n.sobolev@gmail.com>
2017-11-01 22:59:42 +0500

    Bumped version and updated cythonized C file

--- DFTXMLParser/__init__.py
+++ DFTXMLParser/__init__.py
@@ -3,4 +3,4 @@
 # This file is a part of DFTXMLParser project. The project is licensed under the MIT license.
 # See the LICENSE file in the project root for license terms.
 
-__version__ = "0.1"
+__version__ = "0.2"
